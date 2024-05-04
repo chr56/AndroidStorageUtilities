fc4aeddac3f11d1add1897e182bc0cd42d1d60fd
chr_56 <chr0056@gmail.com>
2024-05-04 13:27:28 +0800

    [PermissionDelegate] rename

---
+++ storage/src/main/java/lib/activityresultcontract/RequestMultiplePermissionsDelegate.kt
@@ -0,0 +1,18 @@
+/*
+ *  Copyright (c) 2022~2024 chr_56
+ */
+
+package lib.activityresultcontract
+
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+
+class RequestMultiplePermissionsDelegate : ActivityResultLauncherDelegate<Array<String>, Map<String, Boolean>>() {
+    override val key: String = "RequestMultiplePermissionsTool"
+    override val contract: ActivityResultContract<Array<String>, Map<String, Boolean>> =
+        ActivityResultContracts.RequestMultiplePermissions()
+}
+
+interface IRequestMultiplePermission {
+    val requestMultiplePermissionsDelegate: RequestMultiplePermissionsDelegate
+}
---
+++ storage/src/main/java/lib/activityresultcontract/RequestPermissionDelegate.kt
@@ -0,0 +1,17 @@
+/*
+ *  Copyright (c) 2022~2024 chr_56
+ */
+
+package lib.activityresultcontract
+
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+
+class RequestPermissionDelegate : ActivityResultLauncherDelegate<String, Boolean>() {
+    override val key: String = "RequestPermissionTool"
+    override val contract: ActivityResultContract<String, Boolean> = ActivityResultContracts.RequestPermission()
+}
+
+interface IRequestPermission {
+    val requestPermissionDelegate: RequestPermissionDelegate
+}
